6ffd72d2a55f3bb1874fa4dde7095ebb855e9354
Hans Meine <meine@informatik.uni-hamburg.de>
2002-10-22 11:56:07 +0200

    some more obvious variable names

--- src/cellimage/testconfigurations.cxx
+++ src/cellimage/testconfigurations.cxx
@@ -4,47 +4,47 @@
 
 int main()
 {
-    int i = 0;
-    
-    cerr << "Test: " << cellConfigurations[0] << ' ' << 
+    int configuration = 0;
+
+    cerr << "Test: " << cellConfigurations[0] << ' ' <<
                         cellConfigurations[255] << endl;
-    
-    for(; i<256; ++i)
+
+    for(; configuration<256; ++configuration)
     {
         int k;
-        
+
         int bits[8];
         for(k=0; k<8; ++k)
         {
-            bits[k] = (i >> k) & 1;
+            bits[k] = (configuration >> k) & 1;
         }
-        
-        int ir  = bits[0] | 
-                 (bits[7] << 1) |
-                 (bits[6] << 2) |
-                 (bits[5] << 3) |
-                 (bits[4] << 4) |
-                 (bits[3] << 5) |
-                 (bits[2] << 6) |
-                 (bits[1] << 7);
-        
-        int r = ir;
-        int o = i;
-        
+
+        int reflected  =  bits[0] |
+						 (bits[7] << 1) |
+						 (bits[6] << 2) |
+						 (bits[5] << 3) |
+						 (bits[4] << 4) |
+						 (bits[3] << 5) |
+						 (bits[2] << 6) |
+						 (bits[1] << 7);
+
+        int r = reflected;
+        int o = configuration;
+
         for(k=0; k<4; ++k)
         {
-            if(cellConfigurations[i] != cellConfigurations[o])
+            if(cellConfigurations[configuration] != cellConfigurations[o])
             {
-                cerr << i << ' ' << o << endl;
+                cerr << configuration << ' ' << o << endl;
             }
-            if(cellConfigurations[i] != cellConfigurations[r])
+            if(cellConfigurations[configuration] != cellConfigurations[r])
             {
-                cerr << i << ' ' << r << endl;
+                cerr << configuration << ' ' << r << endl;
             }
 
             int i2 = ((o << 2) | (o >> 6)) & 0xff;
             o = i2;
-            
+
             i2 = ((r << 2) | (r >> 6)) & 0xff;
             r = i2;
         }
