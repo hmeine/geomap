f6c70626f53f11c593423dec240738513a661133
Hans Meine <meine@informatik.uni-hamburg.de>
2002-11-25 20:03:42 +0100

    partial template specialization for char case: output them after int-typecast

--- src/cellimage/debugimage.hxx
+++ src/cellimage/debugimage.hxx
@@ -5,17 +5,37 @@
 
 namespace vigra {
 
-template <class SrcIter, class SrcAcc>
-void debugImage(SrcIter sul, SrcIter slr, SrcAcc sa,
-                std::ostream &outs = std::cerr)
+template <class SrcIter, class SrcAcc, class ValueType>
+void debugImageImpl(SrcIter sul, SrcIter slr, SrcAcc sa,
+                    ValueType, std::ostream &outs)
 {
-    int w= (slr-sul).x, h= (slr-sul).y;
     for(SrcIter row= sul; row.y<slr.y; row.y++)
     {
         for(SrcIter it= row; it.x<slr.x; it.x++)
             outs << sa(it);
         outs << std::endl;
     }
+}
+
+// partial specialization for chars to output them as int
+template <class SrcIter, class SrcAcc>
+void debugImageImpl(SrcIter sul, SrcIter slr, SrcAcc sa,
+                    unsigned char, std::ostream &outs)
+{
+    for(SrcIter row= sul; row.y<slr.y; row.y++)
+    {
+        for(SrcIter it= row; it.x<slr.x; it.x++)
+            outs << (int)sa(it);
+        outs << std::endl;
+    }
+}
+
+template <class SrcIter, class SrcAcc>
+void debugImage(SrcIter sul, SrcIter slr, SrcAcc sa,
+                std::ostream &outs = std::cerr)
+{
+    if(sul != slr)
+        debugImageImpl(sul, slr, sa, *sul, outs);
 }
 
 template <class SrcIter, class SrcAcc>
