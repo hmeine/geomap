70f37ad60b414ba9e7f9e097c7c5b8a9dabcc97f
Hans Meine <meine@informatik.uni-hamburg.de>
2002-11-25 20:58:33 +0100

    FilterIterator converted to work on top of 1D iterators

--- src/geomap/filteriterator.hxx
+++ src/geomap/filteriterator.hxx
@@ -6,70 +6,47 @@
 
 namespace vigra {
 
-template<class Predicate, class ImageIterator,
-         class Accessor = typename IteratorTraits<ImageIterator>::DefaultAccessor>
+template<class Iterator, class Predicate>
 class FilterIterator
 {
-    ImageIterator lowerRight_, iter_;
-    Accessor ac_;
+    Iterator iter_, end_;
     Predicate predicate_;
-    int width_;
 
 public:
         /** the iterator's value type
         */
-    typedef typename ImageIterator::value_type value_type;
+    typedef typename Iterator::value_type value_type;
 
         /** the iterator's reference type (return type of <TT>*iter</TT>)
         */
-    typedef typename ImageIterator::reference reference;
+    typedef typename Iterator::reference reference;
 
         /** the iterator's pointer type (return type of <TT>operator-></TT>)
         */
-    typedef typename ImageIterator::pointer pointer;
+    typedef typename Iterator::pointer pointer;
 
         /** the iterator tag (forward_iterator_tag)
         */
     typedef std::forward_iterator_tag iterator_category;
 
+    typedef Iterator adaptee;
+
     FilterIterator()
     {}
 
-    FilterIterator(ImageIterator upperLeft, ImageIterator lowerRight,
-                   Accessor ac = Accessor(),
+    FilterIterator(Iterator begin, Iterator end,
                    Predicate predicate = Predicate())
-        : iter_(upperLeft), lowerRight_(lowerRight), ac_(ac),
-          predicate_(predicate), width_(lowerRight.x - upperLeft.x)
+        : iter_(begin), end_(end), predicate_(predicate)
     {
-        if((iter_ != lowerRight_) && !predicate_(ac_(iter_)))
-            operator++();
-    }
-
-    FilterIterator(triple<ImageIterator, ImageIterator, Accessor> src,
-                   Predicate predicate = Predicate())
-        : iter_(src.first), lowerRight_(src.second), ac_(src.third),
-          predicate_(predicate), width_(src.second.x - src.first.x)
-    {
-        if((iter_ != lowerRight_) && !predicate_(ac_(iter_)))
+        if((iter_ != end_) && !predicate_(*iter_))
             operator++();
     }
 
     FilterIterator & operator++()
     {
-        ++iter_.x;
-        while((iter_.x != lowerRight_.x) && !predicate_(ac_(iter_)))
-            ++iter_.x;
-
-        if(iter_.x == lowerRight_.x)
-        {
-            iter_.x -= width_ + 1;
-            ++iter_.y;
-
-            if(iter_.y != lowerRight_.y)
-                operator++();
-            else
-                iter_ = lowerRight_;
-        }
+        ++iter_;
+        while((iter_ != end_) && !predicate_(*iter_))
+            ++iter_;
         return *this;
     }
 
@@ -86,7 +63,7 @@ public:
      */
 	bool atEnd() const
 	{
-		return iter_ == lowerRight_;
+		return iter_ == end_;
 	}
 
     /**
@@ -94,7 +71,7 @@ public:
      */
 	bool inRange() const
 	{
-		return iter_ != lowerRight_;
+		return iter_ != end_;
 	}
 
 	bool operator==(FilterIterator const &other) const
@@ -105,6 +82,16 @@ public:
     bool operator!=(FilterIterator const &other) const
     {
         return iter_ != other.iter_;
+    }
+
+	bool operator==(Iterator const &other) const
+    {
+        return iter_ == other;
+    }
+
+    bool operator!=(Iterator const &other) const
+    {
+        return iter_ != other;
     }
 
     reference operator*() const
