4f1b22d53b0eb7ffa2104b559ba3695938d8a82c
Hans Meine <meine@informatik.uni-hamburg.de>
2002-11-29 14:22:01 +0100

    put Rect2D in a separate file for now (will go into utilities probably) and added Point2D and Size2D

--- src/cellimage/testrect.cxx
+++ src/cellimage/testrect.cxx
@@ -1,4 +1,4 @@
-#include "cellimage.hxx"
+#include "rect2d.hxx"
 #include <vigra/stdimagefunctions.hxx>
 #include <unittest.hxx>
 #include <algorithm>
@@ -12,7 +12,25 @@ namespace vigra {
     }
 }
 
+// template <class T> int (* f (T))[sizeof (T()+2)] { }
+// f(42);
+
 using namespace vigra;
+
+struct Point2DTest
+{
+    Point2D p11;
+
+    Point2DTest()
+        : p11(1, 1)
+    {
+    }
+
+    void testOperations()
+    {
+        should(-p11 == Point2D(-1, -1));
+    }
+};
 
 struct Rect2DTest
 {
@@ -21,7 +39,7 @@ struct Rect2DTest
     Rect2D bigRect;
 
     Rect2DTest()
-        : rect1_1(Diff2D(1, 1), Diff2D(2, 2)),
+        : rect1_1(Point2D(1, 1), Point2D(2, 2)),
           bigRect(0, 0, 10, 10)
     {
     }
@@ -43,56 +61,56 @@ struct Rect2DTest
         should(rect1_1 != emptyRect);
         should(bigRect != emptyRect);
         should(bigRect != rect1_1);
- 
+
         bigRect = rect1_1;
         should(bigRect == rect1_1);
     }
 
     void testContains()
     {
-        should(!emptyRect.contains(Diff2D(0, 0)));
-        should(!emptyRect.contains(Diff2D(0, 1)));
-        should(!emptyRect.contains(Diff2D(0, 2)));
-        should(!emptyRect.contains(Diff2D(1, 0)));
-        should(!emptyRect.contains(Diff2D(1, 1)));
-        should(!emptyRect.contains(Diff2D(1, 2)));
-        should(!emptyRect.contains(Diff2D(2, 0)));
-        should(!emptyRect.contains(Diff2D(2, 1)));
-        should(!emptyRect.contains(Diff2D(2, 2)));
+        should(!emptyRect.contains(Point2D(0, 0)));
+        should(!emptyRect.contains(Point2D(0, 1)));
+        should(!emptyRect.contains(Point2D(0, 2)));
+        should(!emptyRect.contains(Point2D(1, 0)));
+        should(!emptyRect.contains(Point2D(1, 1)));
+        should(!emptyRect.contains(Point2D(1, 2)));
+        should(!emptyRect.contains(Point2D(2, 0)));
+        should(!emptyRect.contains(Point2D(2, 1)));
+        should(!emptyRect.contains(Point2D(2, 2)));
 
         should( emptyRect.contains(emptyRect));
         should(!emptyRect.contains(rect1_1));
         should(!emptyRect.contains(bigRect));
 
-        should(!rect1_1.contains(Diff2D(0, 0)));
-        should(!rect1_1.contains(Diff2D(0, 1)));
-        should(!rect1_1.contains(Diff2D(0, 2)));
-        should(!rect1_1.contains(Diff2D(1, 0)));
-        should( rect1_1.contains(Diff2D(1, 1)));
-        should(!rect1_1.contains(Diff2D(1, 2)));
-        should(!rect1_1.contains(Diff2D(2, 0)));
-        should(!rect1_1.contains(Diff2D(2, 1)));
-        should(!rect1_1.contains(Diff2D(2, 2)));
+        should(!rect1_1.contains(Point2D(0, 0)));
+        should(!rect1_1.contains(Point2D(0, 1)));
+        should(!rect1_1.contains(Point2D(0, 2)));
+        should(!rect1_1.contains(Point2D(1, 0)));
+        should( rect1_1.contains(Point2D(1, 1)));
+        should(!rect1_1.contains(Point2D(1, 2)));
+        should(!rect1_1.contains(Point2D(2, 0)));
+        should(!rect1_1.contains(Point2D(2, 1)));
+        should(!rect1_1.contains(Point2D(2, 2)));
 
         should( rect1_1.contains(emptyRect));
         should( rect1_1.contains(rect1_1));
         should(!rect1_1.contains(bigRect));
 
-        should(bigRect.contains(Diff2D(0, 0)));
-        should(bigRect.contains(Diff2D(0, 1)));
-        should(bigRect.contains(Diff2D(0, 2)));
-        should(bigRect.contains(Diff2D(1, 0)));
-        should(bigRect.contains(Diff2D(1, 1)));
-        should(bigRect.contains(Diff2D(1, 2)));
-        should(bigRect.contains(Diff2D(2, 0)));
-        should(bigRect.contains(Diff2D(2, 1)));
-        should(bigRect.contains(Diff2D(2, 2)));
+        should(bigRect.contains(Point2D(0, 0)));
+        should(bigRect.contains(Point2D(0, 1)));
+        should(bigRect.contains(Point2D(0, 2)));
+        should(bigRect.contains(Point2D(1, 0)));
+        should(bigRect.contains(Point2D(1, 1)));
+        should(bigRect.contains(Point2D(1, 2)));
+        should(bigRect.contains(Point2D(2, 0)));
+        should(bigRect.contains(Point2D(2, 1)));
+        should(bigRect.contains(Point2D(2, 2)));
 
         should( bigRect.contains(emptyRect));
         should( bigRect.contains(rect1_1));
         should( bigRect.contains(bigRect));
     }
-    
+
     void testIntersection()
     {
         should(!emptyRect.intersects(emptyRect));
@@ -105,23 +123,45 @@ struct Rect2DTest
         should( bigRect.intersects(rect1_1));
         should( bigRect.intersects(bigRect));
 
-        should(!bigRect.intersects(Rect2D(Diff2D(3, -3), Diff2D(3, 3))));
-        should( bigRect.intersects(Rect2D(Diff2D(3, -3), Diff2D(4, 3))));
-        should( bigRect.intersects(Rect2D(Diff2D(3, -3), Diff2D(14, 3))));
+        should(!bigRect.intersects(Rect2D(Point2D(3, -3), Point2D(3, 3))));
+        should( bigRect.intersects(Rect2D(Point2D(3, -3), Point2D(4, 3))));
+        should( bigRect.intersects(Rect2D(Point2D(3, -3), Point2D(14, 3))));
 
         should((rect1_1 & emptyRect).isEmpty());
         should(!(rect1_1 & bigRect).isEmpty());
         should((rect1_1 & bigRect) == rect1_1);
     }
-    
+
     void testUnion()
     {
         should(!(rect1_1 | emptyRect).isEmpty());
         should((rect1_1 | emptyRect) == rect1_1);
         should((rect1_1 | bigRect) == bigRect);
-        rect1_1 |= Diff2D(3, 3);
+        rect1_1 |= Point2D(3, 3);
         shouldEqual(rect1_1.upperLeft(), Diff2D(1, 1));
         shouldEqual(rect1_1.lowerRight(), Diff2D(4, 4));
+    }
+
+    void testSizes()
+    {
+        shouldEqual(rect1_1.size(), Size2D(1, 1));
+        shouldEqual(bigRect.size(), Size2D(10, 10));
+        emptyRect.setSize(10, 10);
+        should(bigRect == emptyRect);
+        emptyRect.addSize(Size2D(-4, -7));
+        shouldEqual(emptyRect.size(), Size2D(6, 3));
+        emptyRect.setSize(bigRect.size());
+        should(bigRect == emptyRect);
+    }
+};
+
+struct Point2DTestSuite
+: public test_suite
+{
+    Point2DTestSuite()
+    : test_suite("Point2DTestSuite")
+    {
+        add( testCase( &Point2DTest::testOperations));
     }
 };
 
@@ -134,6 +174,7 @@ struct Rect2DTestSuite
         add( testCase( &Rect2DTest::testProperties));
         add( testCase( &Rect2DTest::testIntersection));
         add( testCase( &Rect2DTest::testUnion));
+        add( testCase( &Rect2DTest::testSizes));
     }
 };
 
@@ -142,5 +183,8 @@ int main(int argc, char **argv)
     Rect2DTestSuite rect2dTestSuite;
     int failed = rect2dTestSuite.run();
     std::cout << rect2dTestSuite.report() << std::endl;
+    Point2DTestSuite point2dTestSuite;
+    failed += point2dTestSuite.run();
+    std::cout << point2dTestSuite.report() << std::endl;
     return failed;
 }
