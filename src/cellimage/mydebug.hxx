fd1ee4d4b4910a1670e74c88c3c62015816ae801
Hans Meine <meine@informatik.uni-hamburg.de>
2002-12-05 12:14:34 +0100

    - added operator<< for Point2D, Size2D, and Rect2D - CellPixel-operator checks field width to reuse it

--- src/cellimage/mydebug.hxx
+++ src/cellimage/mydebug.hxx
@@ -1,27 +1,55 @@
 #ifndef MYDEBUG_HXX
 #define MYDEBUG_HXX
 
-#include "foureightsegmentation.hxx"
+#include <iostream>
+#include <iomanip>
+#include "cellimage.hxx"
 
 std::ostream &operator <<(std::ostream &s, vigra::Diff2D const &d)
 {
-    s << "(" << d.x << "," << d.y << ")";
+    s << "(" << d.x << "/" << d.y << ")";
+    return s;
 }
 
-std::ostream &operator <<(std::ostream &s,
-                          vigra::FourEightSegmentation::CellPixel const &p)
+std::ostream &operator <<(std::ostream &s, vigra::Point2D const &d)
 {
+    s << "(" << d.x << "," << d.y << ")";
+    return s;
+}
+
+std::ostream &operator <<(std::ostream &s, vigra::Size2D const &d)
+{
+    s << "(" << d.x << "x" << d.y << ")";
+    return s;
+}
+
+std::ostream &operator <<(std::ostream &s, vigra::Rect2D const &r)
+{
+    s << "[" << r.upperLeft() << " to " << r.lowerRight()
+      << " = " << r.size() << "]";
+    return s;
+}
+
+namespace vigra { namespace CellImage {
+
+std::ostream &operator <<(std::ostream &s,
+                          const CellPixel &p)
+{
+    std::streamsize width= s.width();
     switch(p.type())
     {
     case CellTypeRegion:
-        s << "\033[1;34m" << p.label() << "\033[0m";
+        s << "\033[1;34m" << std::setw(width) << p.label() << "\033[0m";
         break;
     case CellTypeLine:
         s << p.label();
         break;
     default:
-        s << "\033[1;31m" << p.label() << "\033[0m";
+        s << "\033[1;31m" << std::setw(width) << p.label() << "\033[0m";
     }
+    return s;
 }
+
+} }
 
 #endif // MYDEBUG_HXX
