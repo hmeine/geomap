5ceda107f2758e8dbf16d8bc12781d5b823b1945
Hans Meine <meine@informatik.uni-hamburg.de>
2002-12-05 12:13:30 +0100

    added 'width' param to format output

--- src/cellimage/debugimage.hxx
+++ src/cellimage/debugimage.hxx
@@ -2,17 +2,18 @@
 #define VIGRA_DEBUGIMAGE_HXX
 
 #include <iostream>
+#include <iomanip>
 
 namespace vigra {
 
 template <class SrcIter, class SrcAcc, class ValueType>
 void debugImageImpl(SrcIter sul, SrcIter slr, SrcAcc sa,
-                    ValueType, std::ostream &outs)
+                    ValueType, std::ostream &outs, int width)
 {
     for(SrcIter row= sul; row.y<slr.y; row.y++)
     {
         for(SrcIter it= row; it.x<slr.x; it.x++)
-            outs << sa(it);
+            outs << std::setw(width) << sa(it);
         outs << std::endl;
     }
 }
@@ -20,29 +21,31 @@ void debugImageImpl(SrcIter sul, SrcIter slr, SrcAcc sa,
 // partial specialization for chars to output them as int
 template <class SrcIter, class SrcAcc>
 void debugImageImpl(SrcIter sul, SrcIter slr, SrcAcc sa,
-                    unsigned char, std::ostream &outs)
+                    unsigned char, std::ostream &outs, int width)
 {
     for(SrcIter row= sul; row.y<slr.y; row.y++)
     {
         for(SrcIter it= row; it.x<slr.x; it.x++)
-            outs << (int)sa(it);
+            outs << std::setw(width) << (int)sa(it);
         outs << std::endl;
     }
 }
 
 template <class SrcIter, class SrcAcc>
 void debugImage(SrcIter sul, SrcIter slr, SrcAcc sa,
-                std::ostream &outs = std::cerr)
+                std::ostream &outs = std::cerr, int width= 1)
 {
     if(sul != slr)
-        debugImageImpl(sul, slr, sa, *sul, outs);
+        debugImageImpl(sul, slr, sa, *sul, outs, width);
+    else
+        outs << "(empty image range)\n";
 }
 
 template <class SrcIter, class SrcAcc>
 void debugImage(triple<SrcIter, SrcIter, SrcAcc> src,
-                std::ostream &outs = std::cerr)
+                std::ostream &outs = std::cerr, int width= 1)
 {
-    debugImage(src.first, src.second, src.third, outs);
+    debugImage(src.first, src.second, src.third, outs, width);
 }
 
 } // namespace vigra
