c92cbf2a37db32a1d1987db2a921d2f6e5a242b3
Hans Meine <meine@informatik.uni-hamburg.de>
2002-12-05 18:41:22 +0100

    testing both 1D and 2D filteriterators now

--- src/cellimage/testiterator.cxx
+++ src/cellimage/testiterator.cxx
@@ -1,4 +1,5 @@
 #include "filteriterator.hxx"
+#include "imagefilteriterator.hxx"
 #include <vigra/stdimagefunctions.hxx>
 #include <unittest.hxx>
 #include <algorithm>
@@ -17,6 +18,62 @@ struct FilterOdd : public std::unary_function<int, bool>
     bool operator()(int v) const
     {
         return v%2 != 0;
+    }
+};
+
+struct ImageFilterIteratorTest
+{
+    vigra::IImage img;
+
+    ImageFilterIteratorTest()
+        : img(5, 4)
+    {
+        static const int imgData[] =
+            { 0, 1, 2, 3, 0,
+              1, 2, 4, 0, 1,
+              2, 6, 0, 7, 2,
+              3, 5, 1, 2, 3 };
+
+        std::copy(imgData, imgData + 5*4, img.begin());
+    }
+
+    void testInRangeFilteringEven()
+    {
+        int evenData[] = { 0, 2, 0, 2, 4, 0, 2, 6, 0, 2, 2 };
+        int *even= evenData;
+        for(vigra::ImageFilterIterator<FilterEven, vigra::IImage::const_traverser>
+                fit(srcImageRange(img));
+            fit.inRange(); fit++, even++)
+        {
+            should(even < evenData + sizeof(evenData) / sizeof(evenData[0]));
+            shouldEqual(*fit, *even);
+        }
+        should(even == evenData + sizeof(evenData) / sizeof(evenData[0]));
+    }
+
+    void testAtEndFilteringOdd()
+    {
+        int oddData[] = { 1, 3, 1, 1, 7, 3, 5, 1, 3 };
+        int *odd= oddData;
+        for(vigra::ImageFilterIterator<FilterOdd, vigra::IImage::const_traverser>
+                fit(img.upperLeft(), img.lowerRight());
+            !fit.atEnd(); fit++, odd++)
+        {
+            should(odd < oddData + sizeof(oddData) / sizeof(oddData[0]));
+            shouldEqual(*fit, *odd);
+        }
+        should(odd == oddData + sizeof(oddData) / sizeof(oddData[0]));
+    }
+};
+
+struct ImageFilterIteratorTestSuite
+: public vigra::test_suite
+{
+    ImageFilterIteratorTestSuite()
+    : vigra::test_suite("ImageFilterIteratorTestSuite")
+    {
+        add( testCase( &ImageFilterIteratorTest::testInRangeFilteringEven));
+        add( testCase( &ImageFilterIteratorTest::testAtEndFilteringOdd));
     }
 };
 
@@ -40,8 +97,8 @@ struct FilterIteratorTest
     {
         int evenData[] = { 0, 2, 0, 2, 4, 0, 2, 6, 0, 2, 2 };
         int *even= evenData;
-        for(vigra::FilterIterator<FilterEven, vigra::IImage::const_traverser>
-                fit(srcImageRange(img));
+        for(vigra::FilterIterator<vigra::IImage::ScanOrderIterator, FilterEven>
+                fit(img.begin(), img.end());
             fit.inRange(); fit++, even++)
         {
             should(even < evenData + sizeof(evenData) / sizeof(evenData[0]));
@@ -54,8 +111,8 @@ struct FilterIteratorTest
     {
         int oddData[] = { 1, 3, 1, 1, 7, 3, 5, 1, 3 };
         int *odd= oddData;
-        for(vigra::FilterIterator<FilterOdd, vigra::IImage::const_traverser>
-                fit(img.upperLeft(), img.lowerRight());
+        for(vigra::FilterIterator<vigra::IImage::ScanOrderIterator, FilterOdd>
+                fit(img.begin(), img.end());
             !fit.atEnd(); fit++, odd++)
         {
             should(odd < oddData + sizeof(oddData) / sizeof(oddData[0]));
@@ -78,8 +135,13 @@ struct FilterIteratorTestSuite
 
 int main(int argc, char **argv)
 {
-    FilterIteratorTestSuite filterIteratorTestSuite;
-    int failed = filterIteratorTestSuite.run();
-    std::cout << filterIteratorTestSuite.report() << std::endl;
+    ImageFilterIteratorTestSuite imageFilterIteratorTestSuite;
+    int failed = imageFilterIteratorTestSuite.run();
+    std::cout << imageFilterIteratorTestSuite.report() << std::endl;
+
+    FilterIteratorTestSuite FilterIteratorTestSuite;
+    failed += FilterIteratorTestSuite.run();
+    std::cout << FilterIteratorTestSuite.report() << std::endl;
+
     return failed;
 }
