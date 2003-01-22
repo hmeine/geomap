06cea93d4f75caebd997a7889fd8919af9775f55
Hans Meine <meine@informatik.uni-hamburg.de>
2003-01-22 18:06:49 +0100

    introducing a CellPyramid template which proxies Euler Operation calls and calls to the composed transforms it offeres and logs them in a history.  you can have it store checkpoints for faster traversal of its levels.  it also features a CellStatistics template parameter which takes some kind of policy which makes it possible (together with a constructor parameter) to manage statistics of the cells for each level (there are hooks for updating it during Euler Operations).

---
+++ src/cellimage/cellpyramid.hxx
@@ -0,0 +1,215 @@
+#ifndef CELLPYRAMID_HXX
+#define CELLPYRAMID_HXX
+
+namespace vigra {
+
+template<class Segmentation, class CellStatistics>
+class CellPyramid
+{
+    struct Level
+    {
+        unsigned int level;
+        Segmentation segmentation;
+    };
+
+    typedef typename Segmentation::CellInfo CellInfo;
+    typedef typename Segmentation::NodeInfo NodeInfo;
+    typedef typename Segmentation::EdgeInfo EdgeInfo;
+    typedef typename Segmentation::FaceInfo FaceInfo;
+    typedef typename Segmentation::DartTraverser DartTraverser;
+
+    enum OperationType { RemoveIsolatedNode,
+                         MergeFaces,
+                         RemoveBridge,
+                         RemoveEdge,
+                         RemoveEdgeWithEnds };
+
+    struct Operation
+    {
+        OperationType  type_;
+        DartTraverser  param_;
+
+        Operation(OperationType type, DartTraverser param)
+        : type_(type), param_(param) {}
+    };
+
+    typedef std::map<unsigned int, std::pair<Segmentation, CellStatistics> >
+        CheckpointMap;
+    CheckpointMap checkpoints_;
+
+    typedef std::vector<Operation> History;
+    History history_;
+
+    unsigned int currentLevel_;
+    Segmentation currentSegmentation_;
+    CellStatistics   currentCellStatistics_;
+
+    FaceInfo &removeIsolatedNodeInternal(const DartTraverser & dart)
+    {
+        currentCellStatistics_.preRemoveIsolatedNode(dart);
+        FaceInfo &result(currentSegmentation_.removeIsolatedNode(dart));
+        currentCellStatistics_.postRemoveIsolatedNode(dart, result);
+        return result;
+    }
+
+    FaceInfo &mergeFacesInternal(const DartTraverser & dart)
+    {
+        currentCellStatistics_.preMergeFaces(dart);
+        FaceInfo &result(currentSegmentation_.mergeFaces(dart));
+        currentCellStatistics_.postMergeFaces(dart, result);
+        return result;
+    }
+
+    FaceInfo &removeBridgeInternal(const DartTraverser & dart)
+    {
+        currentCellStatistics_.preRemoveBridge(dart);
+        FaceInfo &result(currentSegmentation_.removeBridge(dart));
+        currentCellStatistics_.postRemoveBridge(dart, result);
+        return result;
+    }
+
+    CellInfo &performOperation(Operation &op)
+    {
+        switch(op.type_)
+        {
+          case RemoveIsolatedNode:
+          {
+              return removeIsolatedNodeInternal(op.param_);
+          }
+          case MergeFaces:
+          {
+              return mergeFacesInternal(op.param_);
+          }
+          case RemoveBridge:
+          {
+              return removeBridgeInternal(op.param_);
+          }
+          case RemoveEdge:
+          {
+              return (op.param_.leftFaceLabel() == op.param_.rightFaceLabel() ?
+                      removeBridgeInternal(op.param_) :
+                      mergeFacesInternal(op.param_));
+          }
+          case RemoveEdgeWithEnds:
+          {
+              EdgeInfo &removedEdge = currentSegmentation_.edge(op.param_.edgeLabel());
+
+              FaceInfo &result = (op.param_.leftFaceLabel() == op.param_.rightFaceLabel() ?
+                                  removeBridgeInternal(op.param_) :
+                                  mergeFacesInternal(op.param_));
+
+              if(removedEdge.start.recheckSingularity())
+                  removeIsolatedNodeInternal(removedEdge.start);
+
+              if(removedEdge.end.recheckSingularity())
+                  removeIsolatedNodeInternal(removedEdge.end);
+
+              return result;
+          }
+        }
+
+        vigra_fail("Unknown operation type in CellPyramid<>::performOperation!");
+        return currentSegmentation_.face(0);
+    }
+
+    CellInfo &gotoLevel(unsigned int level)
+    {
+        vigra_precondition(level < levelCount(),
+                           "trying to access non-existing level (too high index)!");
+
+        if(currentLevel_ > level)
+        {
+            typename CheckpointMap::iterator lastCheckpointIt =
+                checkpoints_.upper_bound(level);
+            --lastCheckpointIt;
+            currentLevel_ = lastCheckpointIt->first;
+            currentSegmentation_ = lastCheckpointIt->second.first;
+            currentCellStatistics_ = lastCheckpointIt->second.second;
+        }
+
+        while(currentLevel_ < level)
+        {
+            CellInfo &result= performOperation(history_[currentLevel_]);
+            ++currentLevel_;
+            if(currentLevel_ == level)
+                return result;
+        }
+
+        return currentSegmentation_.face(0); // something HAS to be returned... ;-)
+    }
+
+  public:
+    void storeCheckpoint()
+    {
+        std::cerr << "storeCheckpoint()\n";
+        checkpoints_.insert(std::make_pair(levelCount()-1, std::make_pair(currentSegmentation_, currentCellStatistics_)));
+    }
+
+    CellPyramid(const Segmentation &level0,
+                const CellStatistics &level0Stats = CellStatistics())
+    : currentSegmentation_(level0),
+      currentCellStatistics_(level0Stats)
+    {
+        storeCheckpoint();
+    }
+
+    FaceInfo &removeIsolatedNode(const DartTraverser & dart)
+    {
+        history_.push_back(Operation(RemoveIsolatedNode, dart));
+        return static_cast<FaceInfo &>(gotoLevel(levelCount()-1));
+    }
+
+    FaceInfo &mergeFaces(const DartTraverser & dart)
+    {
+        history_.push_back(Operation(MergeFaces, dart));
+        return static_cast<FaceInfo &>(gotoLevel(levelCount()-1));
+    }
+
+    FaceInfo &removeBridge(const DartTraverser & dart)
+    {
+        history_.push_back(Operation(RemoveBridge, dart));
+        return static_cast<FaceInfo &>(gotoLevel(levelCount()-1));
+    }
+
+    FaceInfo &removeEdge(const DartTraverser & dart)
+    {
+        history_.push_back(Operation(RemoveEdge, dart));
+        return static_cast<FaceInfo &>(gotoLevel(levelCount()-1));
+    }
+
+    FaceInfo &removeEdgeWithEnds(const DartTraverser & dart)
+    {
+        history_.push_back(Operation(RemoveEdgeWithEnds, dart));
+        return static_cast<FaceInfo &>(gotoLevel(levelCount()-1));
+    }
+
+    const Segmentation &currentLevel() const
+    {
+        return currentLevel_;
+    }
+
+    const Segmentation &currentSegmentation() const
+    {
+        return currentSegmentation_;
+    }
+
+    const CellStatistics &currentCellStatistics() const
+    {
+        return currentCellStatistics_;
+    }
+
+    const Segmentation &segmentation(unsigned int level)
+    {
+        gotoLevel(level);
+        return currentSegmentation_;
+    }
+
+    unsigned int levelCount() const
+    {
+        return history_.size() + 1;
+    }
+};
+
+} // namespace vigra
+
+#endif // CELLPYRAMID_HXX
