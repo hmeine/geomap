7d35129623c6095638c441cf5a16ca8964d08e04
Hans Meine <meine@informatik.uni-hamburg.de>
2003-02-24 11:30:43 +0100

    offer history, too (and thus Operations and OperationTypes)

--- src/cellimage/cellimage_pyramid.cxx
+++ src/cellimage/cellimage_pyramid.cxx
@@ -1,5 +1,6 @@
 #include "cellimage_module.hxx"
 #include "cellstatistics.hxx"
+#define private public
 #include "cellpyramid.hxx"
 
 using namespace vigra;
@@ -11,8 +12,22 @@ typedef vigra::CellPyramid<FourEightSegmentation, CellStatistics>
 
 using namespace boost::python;
 
+FourEightPyramid::Operation &historyGetItem(FourEightPyramid::History &history,
+											long index)
+{
+	if((index >= (long)history.size()) || (index < -(long)history.size()))
+	{
+		PyErr_SetObject(PyExc_IndexError, vigra::ownedPyObject(index));
+		python::throw_error_already_set();
+	}
+	if(index < 0)
+		index += history.size();
+	return history[index];
+}
+
 void definePyramid()
 {
+	scope fourEightPyramidScope(
 	class_<FourEightPyramid>("FourEightPyramid", no_init)
 		//init<const FourEightSegmentation&>())
 		.def("storeCheckpoint", &FourEightPyramid::storeCheckpoint)
@@ -33,8 +48,26 @@ void definePyramid()
 			 return_internal_reference<>())
 		.def("currentCellStatistics", &FourEightPyramid::currentCellStatistics,
 			 return_internal_reference<>())
-		.def("segmentation", &FourEightPyramid::segmentation,
+		.def("__getitem__", &FourEightPyramid::segmentation,
 			 return_internal_reference<>())
 		.def("cutHead", &FourEightPyramid::cutHead)
-		.def("levelCount", &FourEightPyramid::levelCount);
+		.def("__len__", &FourEightPyramid::levelCount)
+		.def_readonly("history", &FourEightPyramid::history_));
+
+	enum_<FourEightPyramid::OperationType>("OperationType")
+		.value("RemoveIsolatedNode", FourEightPyramid::RemoveIsolatedNode)
+		.value("MergeFaces", FourEightPyramid::MergeFaces)
+		.value("RemoveBridge", FourEightPyramid::RemoveBridge)
+		.value("MergeEdges", FourEightPyramid::MergeEdges)
+		.value("RemoveEdge", FourEightPyramid::RemoveEdge)
+		.value("RemoveEdgeWithEnds", FourEightPyramid::RemoveEdgeWithEnds);
+	
+	class_<FourEightPyramid::Operation>("Operation", no_init)
+		.def_readonly("type", &FourEightPyramid::Operation::type_)
+		.def_readonly("param", &FourEightPyramid::Operation::param_);
+
+	class_<FourEightPyramid::History>("History", no_init)
+		.def("__len__", &FourEightPyramid::History::size)
+		.def("__getitem__", &historyGetItem,
+			 return_internal_reference<>());
 }
