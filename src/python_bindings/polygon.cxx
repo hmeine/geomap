d2abbcfeb7c02476d0e65dde0a4357474edfb48e
Hans Meine <meine@informatik.uni-hamburg.de>
2005-08-10 11:07:42 +0200

    - don't use boost::python::vector_indexing_suite anymore   (too slow / too much bloat) - implement __getitem__, __setitem__, __iter__, __len__, insert,   split, append, extend and so on by hand - export simplifyPolygon

--- src/python_bindings/polygon.cxx
+++ src/python_bindings/polygon.cxx
@@ -1,6 +1,5 @@
 #include <boost/python.hpp>
 #include <boost/python/detail/api_placeholder.hpp>
-#include <boost/python/suite/indexing/vector_indexing_suite.hpp>
 #include "vigra/pythonimage.hxx"
 #include "vigra/polygon.hxx"
 
@@ -13,61 +12,154 @@ using namespace boost::python;
 
 // };
 
-typedef Polygon<Pixel> PythonPolygon;
-
 Point2D intPos(const Pixel &p)
 {
     return Point2D((int)(p[0]+0.5f), (int)(p[1]+0.5f));
 }
 
-template<class Polygon>
-typename Polygon::value_type
-Polygon__getitem__(Polygon const & p, int i)
+inline void checkPointIndex(int &i, unsigned int size)
 {
     if(i < 0)
-        i += p.size();
-    if(i < 0 || i >= p.size())
+        i += size;
+    if(i < 0 || i >= size)
     {
         PyErr_SetString(PyExc_IndexError,
-            "Polygon.__getitem__(): index out of bounds.");
+            "point index out of bounds.");
         throw_error_already_set();
     }
-    return p[i];
+}
+
+template<class Array>
+typename Array::value_type
+Array__getitem__(Array const & a, int i)
+{
+    checkPointIndex(i, a.size());
+    return a[i];
+}
+
+template<class Array>
+void
+Array__setitem__(Array & a, int i, typename Array::value_type v)
+{
+    checkPointIndex(i, a.size());
+    a[i] = v;
+}
+
+template<class Polygon>
+void
+Polygon__setitem__(Polygon & p, int i, typename Polygon::value_type v)
+{
+    checkPointIndex(i, p.size());
+    p.setPoint(i, v);
+}
+
+template<class Polygon>
+void insert(Polygon & p, int pos, typename Polygon::const_reference x)
+{
+    checkPointIndex(pos, p.size());
+    p.insert(p.begin() + pos, x);
+}
+
+template<class Polygon>
+Polygon split(Polygon & p, int pos)
+{
+    checkPointIndex(pos, p.size());
+    return p.split(pos);
+}
+
+template<class Iterator>
+void defIter(const char *name)
+{
+    class_<Iterator>(name, no_init)
+        .def("__len__", &Iterator::__len__)
+        .def("__iter__", &Iterator::__iter__)
+        .def("next", &Iterator::next)
+    ;
+}
+
+template<class Array>
+PointIter<typename Array::const_iterator>
+__iter__(const Array &a)
+{
+    return PointIter<typename Array::const_iterator>(
+        a.begin(), a.end());
+}
+
+template<class Array>
+PointIter<typename Array::const_reverse_iterator>
+__reviter__(const Array &a)
+{
+    return PointIter<typename Array::const_reverse_iterator>(
+        a.rbegin(), a.rend());
+}
+
+template<class Array>
+Array simplifyPolygon(const Array &a, double epsilon)
+{
+    Array result;
+    simplifyPolygon(a, result, epsilon);
+    return result;
 }
 
 void defPolygon()
 {
     def("intPos", &intPos);
 
+    typedef Polygon<Pixel> PythonPolygon;
+
     typedef PythonPolygon::Base FPointArray;
+    typedef PointIter<FPointArray::const_iterator>
+        FPointIter;
+    defIter<FPointIter>("FPointIter");
+    typedef PointIter<FPointArray::const_reverse_iterator>
+        FPointRevIter;
+    defIter<FPointRevIter>("FPointRevIter");
     class_<FPointArray>("VectorArray")
         .def(init<FPointArray>())
-        .def(vector_indexing_suite<FPointArray>())
         .def("reverse", &FPointArray::reverse)
+        .def("__len__", &FPointArray::size)
+        .def("__getitem__", &Array__getitem__<FPointArray>)
+        .def("__setitem__", &Array__setitem__<FPointArray>)
+        .def("__iter__", &__iter__<FPointArray>)
+        .def("__reviter__", &__reviter__<FPointArray>)
+        .def("insert", &insert<FPointArray>)
         .def(self * double())
         .def("roundToInteger", &FPointArray::roundToInteger)
     ;
 
     typedef PointArray<Point2D> IPointArray;
+    typedef PointIter<IPointArray::const_iterator>
+        IPointIter;
+    defIter<IPointIter>("IPointIter");
+    typedef PointIter<IPointArray::const_reverse_iterator>
+        IPointRevIter;
+    defIter<IPointRevIter>("IPointRevIter");
     class_<IPointArray>("Point2DArray")
         .def(init<IPointArray>())
-        .def(vector_indexing_suite<IPointArray>())
+        .def("reverse", &IPointArray::reverse)
+        .def("__len__", &IPointArray::size)
+        .def("__getitem__", &Array__getitem__<IPointArray>)
+        .def("__setitem__", &Array__setitem__<IPointArray>)
+        .def("__iter__", &__iter__<IPointArray>)
+        .def("__reviter__", &__reviter__<IPointArray>)
+        .def("insert", &insert<IPointArray>)
     ;
 
     class_<PythonPolygon, bases<PythonPolygon::Base> >("Polygon")
         .def(init<list>())
         .def(init<FPointArray>())
-        .def("__reviter__", &PythonPolygon::__reviter__)
-        .def("__getitem__", &Polygon__getitem__<PythonPolygon>)
+        .def("insert", &insert<PythonPolygon>)
+        .def("append", &PythonPolygon::push_back)
+        .def("extend", &PythonPolygon::extend)
+        .def("__setitem__", &Polygon__setitem__<PythonPolygon>)
+        .def("split", &split<PythonPolygon>)
         .def("length", &PythonPolygon::length)
         .def("partialArea", &PythonPolygon::partialArea)
         .def("invalidateProperties", &PythonPolygon::invalidateProperties)
     ;
 
-    class_<PythonPolygon::RevPointIter>("RevPointIter", no_init)
-        .def("__len__", &PythonPolygon::RevPointIter::__len__)
-        .def("__iter__", &PythonPolygon::RevPointIter::__iter__)
-        .def("next", &PythonPolygon::RevPointIter::next,
-             return_internal_reference<>())
-    ;
+    def("simplifyPolygon",
+        (PythonPolygon (*)(const PythonPolygon &,double))&simplifyPolygon);
+    def("simplifyPolygon",
+        (FPointArray (*)(const FPointArray &,double))&simplifyPolygon);
 }
