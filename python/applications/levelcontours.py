f98d6f4d7b5501bf1eff42232598caaf2f2151b5
Hans Meine <meine@informatik.uni-hamburg.de>
2006-01-09 09:47:45 +0100

    - fixed more than half a dozen errors introduced with my last commit #-/ - more refactoring (this time, tested! ;-) )

--- python/applications/levelcontours.py
+++ python/applications/levelcontours.py
@@ -1,3 +1,10 @@
+def tangentDir(s, x0, y0):
+    dx, dy = -s.dy(x0, y0), s.dx(x0, y0)
+    norm = sqrt(sq(dx) + sq(dy))
+    dx /= norm
+    dy /= norm
+    return dx, dy
+
 def predictorStep1(s, x0, y0, h):
     dx, dy = -s.dy(x0, y0), s.dx(x0, y0)
     norm = sqrt(sq(dx) + sq(dy))
@@ -30,7 +37,7 @@ def correctorStep2(s, x0, y0, epsilon = 1e-4, n0 = 3):
     return None, None
 
 def predictorCorrectorStep(s, x, y, h, epsilon):
-    while h > 1e-6: # FIXME
+    while abs(h) > 1e-6: # FIXME
         p1 = predictorStep1(s, x, y, h)
         if not p1:
             h /= 2.0
@@ -42,13 +49,41 @@ def predictorCorrectorStep(s, x, y, h, epsilon):
         # use pow(..., 0.5) in connection with predictorStep1(),
         # use pow(..., 0.33) in connection with predictorStep2(),
         h /= min(2.0, max(0.5, pow(contraction, 0.5)))
-        if h > 0.2:
+        if abs(h) > 0.2:
             h = 0.2
         break
-    if h > 1e-6: # FIXME
-        return p2, h
     else:
         return None, None
+    return p2, h
+
+def haralickConstraint(z, i, x, y, t):
+    if i.g2(x, y) < t:
+        return False
+    gx, gy = i.dx(x, y), i.dy(x, y)
+    return (gx**3 * i.dx3(x, y) + \
+            3*gx**2 * gy * i.dxxy(x, y) + \
+            3*gx * gy**2 * i.dxyy(x, y) + \
+            gy**3 * i.dy3(x, y) < 0)
+
+def laplaceConstraint(z, i, x, y, t):
+    if i.g2(x, y) < t:
+        return False
+    gx, gy = -z.dx(x, y), -z.dy(x, y)
+    return (gx**3 * i.dx3(x, y) + \
+            3*gx**2 * gy * i.dxxy(x, y) + \
+            3*gx * gy**2 * i.dxyy(x, y) + \
+            gy**3 * i.dy3(x, y) < 0)
+
+def splineConstraint(z, i, x, y, t):
+    if i(x,y) < t:
+        return False
+#      gx, gy, gxx, gxy, gyy = i.dx(x,y), i.dy(x,y), i.dxx(x,y), i.dxy(x,y), i.dyy(x,y)
+#      l = gy**2 * gxx - 2*gx*gy*gxy + gx**2 * gyy
+#      r = gx**2 * gxx + 2*gx*gy*gxy + gy**2 * gyy
+#     return (l > r and l > 0.0)
+#      return (l <= 0.0)
+    gx, gy, gxx, gxy, gyy = z.dx(x,y), z.dy(x,y), i.dxx(x,y), i.dxy(x,y), i.dyy(x,y)
+    return gx**2 * gxx + 2*gx*gy*gxy + gy**2 * gyy <= 0.0
 
 class ZeroEdges:
     """zero crossings of an image, its oriented second derivative, the
@@ -61,7 +96,9 @@ class ZeroEdges:
         s = SplineImageView5(image)
         self.i = s
         z = GrayImage(image.size())
-        if method is "haralick":
+        if method is "direct":
+            z = image
+        elif method is "haralick":
             for x,y in image.size():
                 z[x,y] =  s.dx(x,y)**2 * s.dxx(x,y) + \
                           2*s.dx(x,y)*s.dy(x,y)*s.dxy(x,y) + \
@@ -69,8 +106,6 @@ class ZeroEdges:
         elif method is "laplace":
             for x,y in image.size():
                 z[x,y] =  s.dxx(x,y) + s.dyy(x,y)
-        elif method is "direct":
-            z = image
         else:
             for x,y in image.size():
                 gx, gy, gxx, gxy, gyy = s.dx(x,y), s.dy(x,y), s.dxx(x,y), s.dxy(x,y), s.dyy(x,y)
@@ -79,49 +114,26 @@ class ZeroEdges:
         self.regions = transformImage(z, '\l x: x > 0? 1: x<0? -1: 0')
         self.m = method
 
-    def _addFacetIntersection(facetX, facetY, point):
+    def _addFacetIntersection(self, facetX, facetY, point):
         facetIndex = facetX + 10000*facetY
         if self.facets.has_key(facetIndex):
-            self.facets[facetIndex].append((xx,yy))
+            self.facets[facetIndex].append(point)
         else:
-            self.facets[facetIndex] = [(xx, yy)]
+            self.facets[facetIndex] = [point]
 
     def points(self, useConstraint = True, t = 1e-7):
-        p = []
+        result = []
         self.facets = {}
 
         if not useConstraint:
             def checkConstraint(z, i, xx, yy, t):
                 return True
         elif self.m == "haralick":
-            def checkConstraint(z, i, xx, yy, t):
-                if i.g2(xx,yy) < t:
-                    return False
-                gx, gy = i.dx(xx,yy), i.dy(xx,yy)
-                return (gx**3 * i.dx3(xx,yy) + \
-                        3*gx**2 * gy * i.dxxy(xx,yy) + \
-                        3*gx * gy**2 * i.dxyy(xx,yy) + \
-                        gy**3 * i.dy3(xx,yy) < 0)
+            checkConstraint = haralickConstraint
         elif self.m in "laplace":
-            def checkConstraint(z, i, xx, yy, t):
-                if i.g2(xx,yy) < t:
-                    return False
-                gx, gy = -z.dx(xx,yy), -z.dy(xx,yy)
-                return (gx**3 * i.dx3(xx,yy) + \
-                        3*gx**2 * gy * i.dxxy(xx,yy) + \
-                        3*gx * gy**2 * i.dxyy(xx,yy) + \
-                        gy**3 * i.dy3(xx,yy) < 0)
+            checkConstraint = laplaceConstraint
         else:
-            def checkConstraint(z, i, x, y, t):
-                if i(x,y) < t:
-                    return False
-#                gx, gy, gxx, gxy, gyy = i.dx(x,y), i.dy(x,y), i.dxx(x,y), i.dxy(x,y), i.dyy(x,y)
-#                l = gy**2 * gxx - 2*gx*gy*gxy + gx**2 * gyy
-#                r = gx**2 * gxx + 2*gx*gy*gxy + gy**2 * gyy
-#               return (l > r and l > 0.0)
-#                return (l <= 0.0)
-                gx, gy, gxx, gxy, gyy = z.dx(x,y), z.dy(x,y), i.dxx(x,y), i.dxy(x,y), i.dyy(x,y)
-                return gx**2 * gxx + 2*gx*gy*gxy + gy**2 * gyy <= 0.0
+            checkConstraint = splineConstraint
 
         for x, y in self.i.size() - Size2D(1, 1):
             c = self.z.coefficients(x, y)
@@ -135,7 +147,7 @@ class ZeroEdges:
                 p = (x + k, y)
                 if not checkConstraint(self.z, self.i, p[0], p[1], t):
                     continue
-                p.append(p)
+                result.append(p)
 
                 self._addFacetIntersection(x, y,   p)
                 self._addFacetIntersection(x, y-1, p)
@@ -147,12 +159,12 @@ class ZeroEdges:
                 p = (x, y + k)
                 if not checkConstraint(self.z, self.i, p[0], p[1], t):
                     continue
-                p.append(p)
+                result.append(p)
 
                 self._addFacetIntersection(x,   y, p)
                 self._addFacetIntersection(x-1, y, p)
 
-        return p
+        return result
 
     def edges(self, useConstraint = True, t = 1e-7):
         try:
@@ -182,24 +194,29 @@ def distinct(l):
     map(setitem, (d,)*len(l), l, [])
     return d.keys()
 
-def levelEdgesMap(edges):
+def levelEdgesMap(edges, imageSize):
     assert len(edges[0]) >= 2 and len(edges[0][0]) == 2, \
            "mapFromZeroEdges() expects output of ZeroEdges.edges() as parameter!"
 
-    nodePositions = [ep[0] for ep in ee]
-    nodePositions.extend([ep[1] for ep in ee])
+    nodePositions = [ep[0] for ep in edges]
+    nodePositions.extend([ep[1] for ep in edges])
 
     nodePositions = distinct(nodePositions)
     nodePositions.insert(0, None)
 
     edgeTriples = [(nodePositions.index(ep[0]), nodePositions.index(ep[1]),
                     [Vector2(*ep[0]), Vector2(*ep[1])])
-                   for ep in ee]
+                   for ep in edges]
     edgeTriples.insert(0, None)
 
     nodePositions = [np and Vector2(*np) for np in nodePositions]
 
-    result = Map(nodePositions, edgeTriples, roi.size(),
-                   performEdgeSplits = False)
+    result = Map(nodePositions, edgeTriples, imageSize,
+                 performEdgeSplits = False)
     removeCruft(result, 2)
     return result
+
+### USAGE EXAMPLE: ###
+# ze = ZeroEdges(transformImage(phi, '\l x: x + %s' % level), "direct")
+# ee = ze.edges(False)
+# levelMap = levelEdgesMap(ee)
