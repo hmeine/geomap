7f7b92f26ba0df8a7bddf26eeb48bbf4c742722f
Hans Meine <meine@informatik.uni-hamburg.de>
2006-02-15 10:10:24 +0100

    test contains() and faceAt() with first, small Map

--- python/test/test.py
+++ python/test/test.py
@@ -12,6 +12,20 @@ map = Map(maxima2, flowlines2, Size2D(39, 39))
 assert checkConsistency(map), "map inconsistent"
 assert checkLabelConsistency(map), "map.labelImage inconsistent"
 
+# merge faces so that survivor has a hole:
+mergeFaces(map.dart(210))
+mergeFaces(map.dart(214))
+mergeFaces(map.dart(28))
+mergeFaces(map.dart(188))
+mergeFaces(map.dart(196))
+mergeFaces(map.dart(222))
+removeBridge(map.dart(31))
+mergeFaces(map.dart(18))
+
+assert len(map.face(1).contours()) > 1
+assert map.face(1).contains(Vector2(5,12))
+assert not map.face(1).contains(Vector2(12,12)) # in hole
+
 map = Map(maxima1, flowlines1, Size2D(256, 256))
 assert checkConsistency(map), "map inconsistent"
 assert checkLabelConsistency(map), "map.labelImage inconsistent"
@@ -20,6 +34,12 @@ assert checkLabelConsistency(map), "map.labelImage inconsistent"
 # showMapStats(map)
 # bg = readImage("../../../Testimages/blox.gif")
 # d = MapDisplay(bg, map)
+
+# --------------------------------------------------------------------
+
+assert map.faceAt(Vector2(91,  86.4)) == map.face(13)
+assert map.faceAt(Vector2(91,  85.8)) == map.face(5)
+assert map.faceAt(Vector2(91.4,85.8)) == map.face(1)
 
 # --------------------------------------------------------------------
 
