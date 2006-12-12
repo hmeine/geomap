998037abcfef2ebc33816f5f0e2326fa4edef436
Hans Meine <meine@informatik.uni-hamburg.de>
2006-12-12 20:09:44 +0100

    - add HessianSIVProxy

--- python/utils/sivtools.py
+++ python/utils/sivtools.py
@@ -8,9 +8,11 @@ def sivByOrder(order):
     return getattr(vigra, "SplineImageView%d" % order)
 
 class GradientSIVProxy(object):
-    def __init__(self, grad, order = 5):
-        self.gxsiv = sivByOrder(order)(grad[0])
-        self.gysiv = sivByOrder(order)(grad[1])
+    def __init__(self, grad, SIV = 5):
+        if type(SIV) == int:
+            SIV = sivByOrder(SIV)
+        self.gxsiv = SIV(grad[0])
+        self.gysiv = SIV(grad[1])
 
     def __getitem__(self, i):
         return Vector2(self.gxsiv[i], self.gysiv[i])
@@ -18,12 +20,21 @@ class GradientSIVProxy(object):
 class ThreeBandSIVProxy(object):
     def __init__(self, image, SIV = 5):
         if type(SIV) == int:
-            SIV = sivByOrder(order)
+            SIV = sivByOrder(SIV)
         self.siv0 = SIV(image[0])
         self.siv1 = SIV(image[1])
         self.siv2 = SIV(image[2])
 
     def __getitem__(self, pos):
         return vigra.Vector(self.siv0[pos], self.siv1[pos], self.siv2[pos])
+
+class HessianSIVProxy(object):
+    def __init__(self, siv):
+        self.siv = siv
+    
+    def __getitem__(self, pos):
+        return vigra.Vector(self.siv.dxx(*pos),
+                            self.siv.dxy(*pos),
+                            self.siv.dyy(*pos))
 
 TensorSIVProxy = ThreeBandSIVProxy
