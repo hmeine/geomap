5d89abf5989858ae6a68af391613186caa86aa3d
Hans Meine <meine@informatik.uni-hamburg.de>
2007-01-26 18:53:09 +0100

    - make DSLs pickle'able - add a,b,pos properties - export LeaningType (necessary for axisIntercept)

--- src/python_bindings/dsl.cxx
+++ src/python_bindings/dsl.cxx
@@ -4,37 +4,57 @@
 #include <boost/python/detail/api_placeholder.hpp>
 #include <boost/python/make_constructor.hpp>
 
+using namespace boost::python;
+using namespace vigra;
+
+template<class DSL>
+struct DSLPickleSuite : pickle_suite
+{
+    static tuple getinitargs(DSL const& dsl)
+    {
+        return make_tuple(dsl.a(), dsl.b(), dsl.pos());
+    }
+};
+
 void defDSL()
 {
-    using namespace boost::python;
+    enum_<DSL::LeaningType>("LeaningType")
+        .value("CenterLine", DSL::CenterLine)
+        .value("LowerLeaningLine", DSL::LowerLeaningLine)
+        .value("UpperLeaningLine", DSL::UpperLeaningLine)
+    ;
 
-    typedef vigra::DigitalStraightLine<int, true> DigitalStraightLine8;
+    typedef DigitalStraightLine<int, true> DigitalStraightLine8;
 
     class_<DigitalStraightLine8>("DigitalStraightLine8", init<int, int, int>())
         .def("eightConnected", &DigitalStraightLine8::eightConnected)
-        .def("a", &DigitalStraightLine8::a)
-        .def("b", &DigitalStraightLine8::b)
-        .def("pos", &DigitalStraightLine8::pos)
+        .add_property("a", &DigitalStraightLine8::a, &DigitalStraightLine8::setA)
+        .add_property("b", &DigitalStraightLine8::b, &DigitalStraightLine8::setB)
+        .add_property("pos", &DigitalStraightLine8::pos, &DigitalStraightLine8::setPos)
+        .def("__call__", &DigitalStraightLine8::operator())
         .def("width", &DigitalStraightLine8::width)
         .def("contains", &DigitalStraightLine8::contains)
         .def("slope", &DigitalStraightLine8::slope)
-        .def("axisIntercept", &DigitalStraightLine8::axisIntercept)
+        .def("axisIntercept", &DigitalStraightLine8::axisIntercept,
+             arg("leaningType") = DSL::CenterLine)
         .def("addPoint", &DigitalStraightLine8::addPoint)
         .def("convertToFourConnected", &DigitalStraightLine8::convertToFourConnected)
+        .def_pickle(DSLPickleSuite<DigitalStraightLine8>())
     ;
 
-    typedef vigra::DigitalStraightLine<int, false> DigitalStraightLine4;
+    typedef DigitalStraightLine<int, false> DigitalStraightLine4;
 
     class_<DigitalStraightLine4>("DigitalStraightLine4", init<int, int, int>())
         .def("eightConnected", &DigitalStraightLine4::eightConnected)
-        .def("a", &DigitalStraightLine4::a)
-        .def("b", &DigitalStraightLine4::b)
-        .def("pos", &DigitalStraightLine4::pos)
+        .add_property("a", &DigitalStraightLine4::a, &DigitalStraightLine4::setA)
+        .add_property("b", &DigitalStraightLine4::b, &DigitalStraightLine4::setB)
+        .add_property("pos", &DigitalStraightLine4::pos, &DigitalStraightLine4::setPos)
+        .def("__call__", &DigitalStraightLine4::operator())
         .def("width", &DigitalStraightLine4::width)
         .def("contains", &DigitalStraightLine4::contains)
         .def("slope", &DigitalStraightLine4::slope)
-        .def("axisIntercept", &DigitalStraightLine4::axisIntercept)
-        .def("addPoint", &DigitalStraightLine4::addPoint)
-        //.def("convertToFourConnected", &DigitalStraightLine4::convertToFourConnected)
+        .def("axisIntercept", &DigitalStraightLine4::axisIntercept,
+             arg("leaningType") = DSL::CenterLine)
+        .def_pickle(DSLPickleSuite<DigitalStraightLine8>())
     ;
 }
