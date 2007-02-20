4bcda0fe8bc9ebd16395af1ec7d3342754091e00
Hans Meine <meine@informatik.uni-hamburg.de>
2007-02-20 12:05:54 +0100

    update to current API and make hole creation really robust

--- python/test/test.py
+++ python/test/test.py
@@ -1,39 +1,46 @@
-from vigra import Vector2
+from vigra import Vector2, Size2D, labelImage4
 from hourglass import Polygon
+import maputils
 
-from map import *
+from hourglass import GeoMap
+#from map import GeoMap
 execfile("testSPWS")
 
 # The following data contains edges that run out of the image range,
 # which ATM leads to overlapping edges after border closing. That
 # violates some assumptions and would lead to errors if we actually
 # worked with that Map.
-map = GeoMap(maxima2, flowlines2, Size2D(39, 39))
-map.sortEdgesEventually(ssStepDist = 0.2, ssMinDist = 0.05)
+map = GeoMap(maxima2, [], Size2D(39, 39))
+maputils.addFlowLinesToMap(flowlines2, map)
+map.sortEdgesEventually(stepDist = 0.2, minDist = 0.05)
+map.splitParallelEdges()
 map.initializeMap()
-# FIXME: performEdgeSplits!!
-assert checkConsistency(map), "map inconsistent"
-assert checkLabelConsistency(map), "map.labelImage() inconsistent"
-assert len(map.history) == 0
+assert map.checkConsistency(), "map inconsistent"
+assert maputils.checkLabelConsistency(map), "map.labelImage() inconsistent"
 
 # merge faces so that survivor has a hole:
-map.mergeFaces(map.dart(218))
-map.mergeFaces(map.dart(-19))
-map.mergeFaces(map.dart(-28))
-map.mergeFaces(map.dart(-33))
-map.mergeFaces(map.dart(-229))
-face = map.removeBridge(map.dart(-31))
+hole = map.faceAt((16,17))
+dart = hole.contour()
+while True:
+    while dart.startNode().degree() > 2:
+        face = maputils.removeEdge(dart.clone().prevSigma())
+    if dart.nextPhi() == hole.contour():
+        break
 
-assert len(face.contours()) > 1 # should have hole
-assert face.contains(Vector2(5,12)) # in region, but not within hole
-assert not face.contains(Vector2(12,12)) # in hole
+assert face.holeCount() > 0 # should have hole
+for p in [(3, 24), (10, 15), (13, 21)]: # in region, but not within hole
+    assert face.contains(p)
+for p in [(16, 17), (13, 17)]: # in hole
+    assert not face.contains(p)
+    assert hole.contains(p)
 
 # --------------------------------------------------------------------
 
 # test copying / init from map data with disconnected contours:
+import copy
 om = copy.copy(map)
-assert checkConsistency(om), "map inconsistent"
-assert checkLabelConsistency(om), "om.labelImage() inconsistent"
+assert om.checkConsistency(), "map inconsistent"
+assert maputils.checkLabelConsistency(om), "om.labelImage() inconsistent"
 
 # --------------------------------------------------------------------
 
@@ -44,18 +51,20 @@ assert cm.faceCount == count + 1
 
 # --------------------------------------------------------------------
 
-map = Map(maxima1, flowlines1, Size2D(256, 256))
-map.sortEdgesEventually(ssStepDist = 0.2, ssMinDist = 0.05)
+map = GeoMap(maxima1, [], Size2D(256, 256))
+maputils.addFlowLinesToMap(flowlines1, map)
+maputils.connectBorderNodes(map, 0.1)
+map.sortEdgesEventually(stepDist = 0.2, minDist = 0.05)
 map.initializeMap()
-assert checkConsistency(map), "map inconsistent"
-assert checkLabelConsistency(map), "map.labelImage() inconsistent"
+assert map.checkConsistency(), "map inconsistent"
+assert maputils.checkLabelConsistency(map), "map.labelImage() inconsistent"
 
 # showMapStats(map)
 # bg = readImage("../../../Testimages/blox.gif")
 # d = MapDisplay(bg, map)
 
-assert map.faceAt(Vector2(91,  86.4)) == map.face(13)
-assert map.faceAt(Vector2(91,  85.8)) == map.face(5)
+assert map.faceAt(Vector2(91,  86.4)) == map.face(14)
+assert map.faceAt(Vector2(91,  85.8)) == map.face(6)
 assert map.faceAt(Vector2(91.4,85.8)) == map.face(1)
 
 # --------------------------------------------------------------------
@@ -68,17 +77,15 @@ def checkPolygons(map):
         p.invalidateProperties()
         if abs(l - p.length()) > 1e-6:
             print "edge %d: length wrong (was %s, is %s)" % (
-                edge._label, l, p.length())
+                edge.label(), l, p.length())
             clean = False
         if abs(pa - p.partialArea()) > 1e-6:
             print "edge %d: partial area wrong (was %s, is %s)" % (
-                edge._label, pa, p.partialArea())
+                edge.label(), pa, p.partialArea())
             clean = False
     return clean
 
 # --------------------------------------------------------------------
-
-backup = copy.copy(map)
 
 import random, time, sys
 if len(sys.argv) > 1:
@@ -93,34 +100,35 @@ random.seed(seed)
 def mergeEdgesCandidates(map):
     result = []
     for node in map.nodeIter():
-        if node.degree() == 2 \
-               and abs(node._darts[0]) != abs(node._darts[1]):
-            result.append(node._label)
+        if node.degree() == 2 and not node.anchor().edge().isLoop():
+            result.append(node.label())
     return result
 
 def removeBridgeCandidates(map):
     result = []
     for edge in map.edgeIter():
         if edge.leftFaceLabel() == edge.rightFaceLabel():
-            result.append(edge._label)
+            result.append(edge.label())
     return result
 
 def mergeFacesCandidates(map):
     result = []
     for edge in map.edgeIter():
         if edge.leftFaceLabel() != edge.rightFaceLabel():
-            result.append(edge._label)
+            result.append(edge.label())
     return result
 
 for node in map.nodeIter():
     if node.degree() == 0:
-        node.uninitialize()
+        map.removeIsolatedNode(node)
+
+backup = copy.copy(map)
 
 history = ""
 try:
   changed = True
   while True:
-    assert checkConsistency(map)
+    assert map.checkConsistency()
 
     if changed:
         possible = range(3)
@@ -142,12 +150,12 @@ try:
             mec.remove(nodeLabel)
             dart = map.node(nodeLabel).anchor()
             print "removing node %d via %s" % (nodeLabel, dart)
-            history += "mergeEdges(map.dart(%d))\n" % (dart.label(), )
+            history += "map.mergeEdges(map.dart(%d))\n" % (dart.label(), )
             mergePos = dart[0]
             survivor = map.mergeEdges(dart)
             if survivor:
-                assert mergePos in [survivor[i] for i in survivor.mergeIndices], \
-                       "mergeIndices do not point to merge position!"
+#                 assert mergePos in [survivor[i] for i in survivor.mergeIndices], \
+#                        "mergeIndices do not point to merge position!"
                 changed = True
 
     if operation == 1:
@@ -160,7 +168,7 @@ try:
             rbc.remove(dartLabel)
             dart = map.dart(dartLabel)
             print "removing bridge via %s" % (dart, )
-            history += "removeBridge(map.dart(%d))\n" % (dartLabel, )
+            history += "map.removeBridge(map.dart(%d))\n" % (dartLabel, )
             if map.removeBridge(dart):
                 changed = True
 
@@ -174,7 +182,7 @@ try:
             mfc.remove(dartLabel)
             dart = map.dart(dartLabel)
             print "removing edge via %s" % (dart, )
-            history += "mergeFaces(map.dart(%d))\n" % (dartLabel, )
+            history += "map.mergeFaces(map.dart(%d))\n" % (dartLabel, )
             if map.mergeFaces(dart):
                 changed = True
 
@@ -182,7 +190,7 @@ except Exception, e:
     print history
     raise
 
-checkLabelConsistency(map)
+maputils.checkLabelConsistency(map)
 
 print "finished successfully, no more candidates for Euler ops:"
 print map.nodeCount, "nodes:", list(map.nodeIter())
@@ -191,9 +199,9 @@ print map.faceCount, "faces:", list(map.faceIter())
 
 # --------------------------------------------------------------------
 
+map = backup
+
 c = time.clock()
-for opName, param in map.history:
-    op = eval(opName)
-    op(backup.dart(param))
+exec history
 
 print "replaying history took %ss." % (time.clock() - c, )
