13d3ace9a92fd9e4011624615545f0c2c2f52014
Hans Meine <meine@informatik.uni-hamburg.de>
2007-03-28 16:15:35 +0200

    - new regression tests - increase size of blox image to 128^2 -> leads to several errors

--- python/test/test.py
+++ python/test/test.py
@@ -6,6 +6,10 @@ from hourglass import GeoMap
 #from map import GeoMap
 execfile("testSPWS")
 
+# --------------------------------------------------------------------
+# 				flowline map creation / face.contains
+# --------------------------------------------------------------------
+
 # The following data contains edges that run out of the image range,
 # which ATM leads to overlapping edges after border closing. That
 # violates some assumptions and would lead to errors if we actually
@@ -35,6 +39,8 @@ for p in [(16, 17), (13, 17)]: # in hole
     assert hole.contains(p)
 
 # --------------------------------------------------------------------
+# 					  copying the map from above
+# --------------------------------------------------------------------
 
 # test copying / init from map data with disconnected contours:
 import copy
@@ -44,37 +50,93 @@ assert om.checkConsistency(), "map inconsistent"
 assert maputils.checkLabelConsistency(om), "om.labelImage() inconsistent"
 
 # --------------------------------------------------------------------
+# 				   simple map, WatershedStatistics
+# --------------------------------------------------------------------
 
-def extractAllSaddles(map):
-    result = []
-    for edge in map.edgeIter():
-        result.extend(map.wsStats.edgeSaddles(edge))
-    return result
-
-from vigra import readImage, resizeImageSplineInterpolation, transformImage, gaussianGradientAsVector, SplineImageView5
+from vigra import readImage, resizeImageSplineInterpolation, transformImage, gaussianGradientAsVector, gaussianGradientMagnitude, SplineImageView5
 from hourglass import SubPixelWatersheds5
 
-img = resizeImageSplineInterpolation(readImage("test_blox.png"), Size2D(64, 64))
-img.grad = gaussianGradientAsVector(img, 1.6)
-img.gm = transformImage(img.grad, "\l x: norm(x)", {})
+img = readImage("test_blox.png")
+img = resizeImageSplineInterpolation(img, Size2D(128, 128))
+img.gm = gaussianGradientMagnitude(img, 1.0)
+# img.grad = gaussianGradientAsVector(img, 1.0)
+# img.gm = transformImage(img.grad, "\l x: norm(x)", {})
 img.gm.siv = SplineImageView5(img.gm)
 img.spws = SubPixelWatersheds5(img.gm)
 
 import maputils
 maxima, flowlines = maputils.subpixelWatershedData(
-    img.spws,
-    img.gm.siv, 0.7)
+    img.spws, img.gm.siv, 0.7, perpendicularDistEpsilon = None)
+
+map = GeoMap(maxima, [], img.size())
+maputils.addFlowLinesToMap(flowlines, map)
+map.sortEdgesEventually(stepDist = 0.2, minDist = 0.05)
+
+def checkSaddles(map, saddles, flowlines = None):
+    print "comparing saddles of %d edges.." % (map.edgeCount, ),
+    valid = True
+    for edge in map.edgeIter():
+        for saddle in map.wsStats.edgeSaddles(edge):
+            if not saddle in saddles:
+                print "illegal saddle %s in %s" % (saddle, edge)
+                valid = False
+                if flowlines:
+                    originalPoly = flowlines[edge.label()][2]
+                    originalSaddleIndex = flowlines[edge.label()][3]
+                    originalSaddle = flowlines[edge.label()][4]
+                    print "  original saddle at index %d/%d: %s = %s" % (
+                        originalSaddleIndex, len(originalPoly),
+                        originalSaddle, saddles[originalSaddle])
+                print "  indices now: %s" % (
+                    map.wsStats._indices[edge.label()], )
+    if valid:
+        print "all OK"
+    return valid
+
+def checkSplittingSaddleHandling(edge, offset):
+    saddles = map.wsStats.edgeSaddles(edge.label())
+    saddleIndex = map.wsStats._indices[edge.label()][0]
+    newEdge = map.splitEdge(edge, saddleIndex + offset)
+    if offset >= 0:
+        assert map.wsStats.edgeSaddles(edge) == saddles
+    if offset <= 0:
+        assert map.wsStats.edgeSaddles(newEdge) == saddles
+
+import statistics
+map.wsStats = statistics.WatershedStatistics(map, flowlines, img.gm.siv)
+assert checkSaddles(map, img.spws.saddles(), flowlines)
+
+from flag_constants import BORDER_PROTECTION
+innerEdges = [edge for edge in map.edgeIter()
+              if not edge.flag(BORDER_PROTECTION)]
+longEdges = [edge for edge in innerEdges
+             if len(edge) > 50 and not edge.isLoop()]
+longLoops = [edge for edge in innerEdges
+             if len(edge) > 50 and edge.isLoop()]
+
+print "checking saddle index handling during splits..."
+checkSplittingSaddleHandling(longEdges[0], 5)
+checkSplittingSaddleHandling(longEdges[1], -5)
+checkSplittingSaddleHandling(longEdges[2], 0)
+checkSplittingSaddleHandling(longLoops[0], 5)
+checkSplittingSaddleHandling(longLoops[1], -5)
+checkSplittingSaddleHandling(longLoops[2], 0)
+print "  OK"
+
+assert checkSaddles(map, img.spws.saddles(), flowlines)
+
+# --------------------------------------------------------------------
+# 			complete subpixel watersheds map + statistics
+# --------------------------------------------------------------------
+
 spmap = maputils.subpixelWatershedMap(
     maxima, flowlines, img.size(),
-
     wsStatsSpline = img.gm.siv,
     minima = img.spws.minima())
 
-print "comparing saddles..",
-saddles1 = img.spws.saddles()
-for saddle in extractAllSaddles(spmap):
-    assert saddle in saddles1
-print "all OK"
+assert checkSaddles(spmap, img.spws.saddles())
+maputils.removeCruft(spmap, 7)
+assert checkSaddles(spmap, img.spws.saddles())
 
 # --------------------------------------------------------------------
 
