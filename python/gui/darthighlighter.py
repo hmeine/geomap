972b1d695aa80f8a7c0ffe74ffa5ed2b89c66fed
Hans Meine <hans_meine@gmx.net>
2015-06-29 16:29:20 +0200

    make DartHighlighter a VigraQt.Overlay

--- python/gui/darthighlighter.py
+++ python/gui/darthighlighter.py
@@ -1,17 +1,19 @@
 import sys
 from weakref import ref
-from PyQt4 import QtCore
-import vigra.pyqt
+from PyQt4 import QtCore, QtGui
+import VigraQt
 
-class DartHighlighter(object):
+class DartHighlighter(VigraQt.Overlay):
     """The DartHighlighter class is attached to a viewer and a Map and
     is able to highlight any set of darts at a time."""
     
     def __init__(self, map, viewer):
+        VigraQt.Overlay.__init__(self, viewer)
         self._map = ref(map)
         self._viewer = viewer
-        self.eo = None
-        self.no = None
+        self._darts = []
+        self._color = QtCore.Qt.yellow
+        viewer.addOverlay(self)
 
     def setMap(self, map):
         self._map = ref(map)
@@ -21,11 +23,11 @@ class DartHighlighter(object):
         Highlight the given darts (can be any iterable returning labels
         or Dart objects)."""
 
+        self._darts = []
         if darts:
             # hasattr(darts, "__iter__"): would be better, but is True for Edges
             if not isinstance(darts, (list, tuple)):
                 darts = [darts]
-            dartObjects = []
             for dart in darts:
                 if type(dart) == int:
                     dart = self._map().dart(dart)
@@ -36,31 +38,23 @@ class DartHighlighter(object):
                     dart = dart.anchor()
                 elif hasattr(dart, "dart"): # Edges
                     dart = dart.dart()
-                dartObjects.append(dart)
-            darts = dartObjects
+                elif hasattr(dart, "contour"): # Faces
+                    dart = dart.contour()
+                self._darts.append(dart)
 
-        if self.eo != None:
-            self._viewer.removeOverlay(self)
-            self.eo = None
-            self.no = None
+        self._viewer.update()
 
-        if darts == None or not len(darts):
+    def draw(self, p, rect):
+        if not self._darts:
             return
 
-        self.eo = vigra.pyqt.EdgeOverlay([dart.edge() for dart in darts], color)
-        self.eo.width = 2
-        self.no = vigra.pyqt.PointOverlay(
-            [dart.startNode().position() for dart in darts], color, 3)
-        self.color = color # used in the viewer's RMB menu
+        p.setPen(self._color)
 
-        self._viewer.addOverlay(self)
-
-    def setZoom(self, zoom):
-        self.eo.viewer = self.viewer
-        self.eo.setZoom(zoom)
-        self.no.viewer = self.viewer
-        self.no.setZoom(zoom)
-
-    def draw(self, p):
-        self.eo.draw(p)
-        self.no.draw(p)
+        p.setBrush(QtGui.QBrush(self._color))
+        w = 2 / self._viewer.zoomFactor()
+        for dart in self._darts:
+            p.drawEllipse(QtCore.QPointF(*dart[0]), w, w)
+            poly = QtGui.QPolygonF(len(dart))
+            for i, (x, y) in enumerate(dart.edge()):
+                poly[i] = QtCore.QPointF(x, y)
+            p.drawPolyline(poly)
